d1c316955b332ded0d54e3aa98a3285303e407fd
adrianodsv <tsiadriano@gmail.com>
2022-09-09 21:52:29 -0300

    Update menu de jogos

--- main.py
+++ main.py
@@ -1,12 +1,13 @@
 import credits
 import adivinhacao
+import forca
 
 def escolhe_jogo():
     print("*********************************")
     print("******AAALO! How you doing?******")
     print("*********************************")
 
-    print("[1] Jogar adivinhação [2] Jogar advinhação (Com resposta) [3] Créditos")
+    print("[1] Jogar adivinhação [2] Jogar advinhação (Com resposta) [3] Jogar Forca [4] Créditos")
 
     opt = int(input("Selecione uma opção: "))
 
@@ -17,6 +18,9 @@ def escolhe_jogo():
         print("Ok então vamos jogar adivinhaçãooo, mas exibindo a resposta")
         adivinhacao.jogar(True)
     elif (opt == 3):
+        print("Ok então vamos jogar Forca!!")
+        forca.jogar()
+    elif (opt == 4):
         print("... segue os créditos")
         credits.show()
 
